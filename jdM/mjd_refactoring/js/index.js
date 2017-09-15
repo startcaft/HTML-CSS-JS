a93a12bc101805b6a98fab36b3c4b7388d423bcd
startcaft <startcaft@startcaft-PC>
2017-09-15 16:25:42 +0800

    重构一个，加入FlexBox布局

---
+++ jdM/mjd_refactoring/js/index.js
@@ -0,0 +1,10 @@
+
+
+window.onload = function (){
+	setTimeout(function(){
+		var scrollDom = document.getElementById('images');
+		scrollDom.style.transform = 'translateX(-10%)';
+		scrollDom.style.webkitTransform = 'translateX(-10%)';
+	},1000)
+}
+
