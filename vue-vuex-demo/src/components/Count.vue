30afb06d6937618c9183b0120377e5575bf7cc04
startcaft <51418996@qq.com>
2017-09-23 10:32:14 +0800

    利用computed特性赋值和mapState访问state访问对象

--- vue-vuex-demo/src/components/Count.vue
+++ vue-vuex-demo/src/components/Count.vue
@@ -2,7 +2,7 @@
 	<div>
         <h2>{{msg}}</h2>
         <hr/>
-        <h3>{{$store.state.count}}</h3>
+        <h3>{{$store.state.count}} - {{count}}</h3>
         <p>
 	    	<button @click="$store.commit('add')">+</button>
 	    	<button @click="$store.commit('reduce')">-</button>
@@ -12,12 +12,30 @@
 
 <script>
 	import store from '@/vuex/store'
+	import { mapState } from 'vuex'			//引入 mapState 
+
 	export default({
 		data : function(){
 			return {
 				msg:'Hello vuex'
 			}
 		},
+		//利用computed特性赋值属性访问state访问对象
+		// computed : {
+		// 	count : function(){
+		// 		return this.$store.state.count;
+		// 	}
+		// },
+
+		//利用mapState对象和computed特性赋值访问state访问对象
+		// computed: mapState({
+		// 	count : function(state){
+		// 		return state.count;
+		// 	}
+		// }),
+
+		//利用mapState数组和computed特性赋值访问state访问对象
+		computed : mapState(['count']),
 		store:store
 	})
 </script>
